9fe90293250fc3333462a1c4fdc9620a255f826b
nagait84 <tatata.friendship.8539.0620@gmail.com>
2014-12-04 17:42:24 +0900

    Add user following2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
            class_name:  "Relationship",
            dependent:   :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :followed_users, through: :relationships, source: :followed
   before_save { self.email = email.downcase }
   before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
