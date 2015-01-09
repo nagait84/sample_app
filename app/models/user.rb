df8fea6d14252040eac7f175e87466366e5781ad
nagait84 <tatata.friendship.8539.0620@gmail.com>
2015-01-09 18:15:22 +0900

    retouch1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
            class_name:  "Relationship",
            dependent:   :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :relationships, foreign_key: "follower_id"
   has_many :followed_users, through: :relationships, source: :followed
   before_save { self.email = email.downcase }
   before_create :create_remember_token
